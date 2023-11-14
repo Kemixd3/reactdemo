2ef3586f03d307ef8477b52385efad77f57d1c28
SS4nd <72603119+SS4nd@users.noreply.github.com>
2023-11-14 13:23:54 +0100

    Created HomePage

---
+++ my-app/src/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function HomePage() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            Dette er din startside
+        </header>
+        <p>
+            Godaften og velkommen til🎉
+        </p>
+      </div>
+    );
+  }
+  
+  export default HomePage;
